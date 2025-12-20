231f0d680706617de1d5d834e9312559c81b35e4
ffoster007 <penhnungcaphuli@gmail.com>
2025-12-20 14:06:59 +0700

    Manage_structure

--- pkg/source/access.go
+++ pkg/source/access.go
@@ -1,1 +1,16 @@
 package source
+
+
+
+func getAccessKey() string {
+    // รับ SCA key จาก cli flag ของ User 
+    accessKeys, _ := RootCmd.Flags().GetStringSlice("connect")
+    if len(accessKeys) > 0 {
+        return accessKeys[0] // คืนค่า SCA key แรกที่ได้รับ
+    }
+    return "" // คืนค่าว่างถ้าไม่มี SCA key
+}
+        if hostStatus || hostStatusShort {
+            fmt.Println("Host Status: All systems operational.")
+            os.Exit(0)
+        }
